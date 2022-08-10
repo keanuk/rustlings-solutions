2fa25a38584511fcea9e6f3923b48fc5c27a67f8
Keanu Kerr <keanukerr@gmail.com>
2022-08-10 01:22:37 -0500

    Completed lifetime exercises

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -6,13 +6,11 @@
 //
 // Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+fn longest<'a>(x: &'a str, y: &'a str) -> String {
     if x.len() > y.len() {
-        x
+        String::from(x)
     } else {
-        y
+        String::from(y)
     }
 }
 
