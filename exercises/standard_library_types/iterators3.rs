e85fe918d433cb6e21bcf76d39fa3b8a68a7acf8
Keanu Kerr <keanukerr@gmail.com>
2022-08-23 20:24:25 -0500

    Update iterators3.rs

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -23,7 +23,16 @@ pub struct NotDivisibleError {
 // Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
-    todo!();
+    if b == 0 {
+        return Err(DivisionError::DivideByZero);
+    }
+    match a % b {
+        0 => Ok(a / b),
+        _ => Err(DivisionError::NotDivisible(NotDivisibleError{
+            dividend: a,
+            divisor: b
+        }))
+    }
 }
 
 // Complete the function and return a value of the correct type so the test passes.
